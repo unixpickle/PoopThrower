188113e121db024ff0c7b834ac67df4ab795bf2d
Alex Nichol <alexnichol@comcast.net>
2012-02-03 21:55:01 -0500

    Initial Commit

---
+++ PoopThrower/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PoopThrower
+//
+//  Created by Alex Nichol on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
